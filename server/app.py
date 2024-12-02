83caf8be469f5114f1c9a1386e6ba38fc4a6b7e9
Victor Hugo <delcarpio.vh@gmail.com>
2024-12-02 16:46:51 -0500

    feat: complete

--- server/app.py
+++ server/app.py
@@ -17,10 +17,64 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        try:
+            plants = Plant.query.all()
+            return [plant.to_dict() for plant in plants], 200
+        except Exception as e:
+            return {'error': str(e)}, 500
+
+    def post(self):
+        try:
+            data = request.get_json()
+            new_plant = Plant(name=data['name'], image=data['image'], price=data['price']) #  Create a new plant
+            db.session.add(new_plant) #  Add the new plant to the session
+            db.session.commit() #  Commit the session
+            return new_plant.to_dict(), 201 #  Return the new plant as JSON with a 201 status code
+        except Exception as e:
+            return {'error': str(e)}, 500
+
+api.add_resource(Plants, '/plants')
 
 class PlantByID(Resource):
-    pass
+    def get(self, id):
+        try:
+            plant = Plant.query.filter(Plant.id == id).first()
+            if plant:
+                return plant.to_dict(), 200
+            else:
+                return {'error': 'Plant not found'}, 404
+        except Exception as e:
+            return {'error': str(e)}, 500
+
+    def post(self, id):
+        try:
+            plant = Plant.query.filter(Plant.id == id).first()
+            if plant:
+                data = request.get_json()
+                plant.name = data['name']
+                plant.image = data['image']
+                plant.price = data['price']
+                db.session.commit()
+                return plant.to_dict(), 200
+            else:
+                return {'error': 'Plant not found'}, 404
+        except Exception as e:
+            return {'error': str(e)}, 500
+
+    def delete(self, id):
+        try:
+            plant = Plant.query.filter(Plant.id == id).first()
+            if plant:
+                db.session.delete(plant)
+                db.session.commit()
+                return {}, 204
+            else:
+                return {'error': 'Plant not found'}, 404
+        except Exception as e:
+            return {'error': str(e)}, 500
+
+api.add_resource(PlantByID, '/plants/<int:id>')
         
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -6,4 +6,8 @@ db = SQLAlchemy()
 class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
+    name = db.Column(db.String)
+    image = db.Column(db.String)
+    price = db.Column(db.Float)
+
     id = db.Column(db.Integer, primary_key=True)
